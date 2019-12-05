8dd21d7bcbd4b891d6a461d3411f198ae4913fdc
Nikita Zarubin <nikita-dimitrov@mail.ru>
2019-12-05 15:14:30 +0300

    Update CRPT.php

--- CRPT.php
+++ CRPT.php
@@ -1,19 +1,28 @@
 <?php
 namespace nikserg\crpt;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use nikserg\cryptoprocli\CryptoProCli;
+use yii\db\Exception;
+
 class CRPT {
     private $test;
     private $jwt;
     private $httpClient;
+    private $certificateThumbprint;
 
     /**
      * CRPT constructor.
      *
+     * @param string $certificateThumbprint SHA1-отпечаток сертификата для входа в ЦРПТ. Пример: 2b0b6f44b3507b509aaa0b2d38dc4819a557a40b
      * @param bool $test Отправлять запросы на тестовый контур ЦРПТ
      */
-    function __construct($test = true)
+    function __construct($certificateThumbprint, $test = true)
     {
         $this->test = $test;
+        $this->certificateThumbprint = $certificateThumbprint;
+        $this->httpClient = new Client();
     }
 
     /**
@@ -36,5 +45,33 @@ class CRPT {
         if ($this->jwt) {
             return true;
         }
+
+        //Получаем данные для подписи
+        $key = @json_decode($this->httpClient->get($this->getCRPTDomain() . 'auth/cert/key/')->getBody(), true);
+
+        //Подписываем
+        $signed = CryptoProCli::signData($key['data'], $this->certificateThumbprint);
+        $signed = str_replace("\n", '', $signed);
+
+        /*$json = json_encode([ 'uuid' => $key['uuid'],
+                              'data' => $signed,]);
+        die($json);*/
+        //Отправляем подписанное
+
+            $jwt = $this->httpClient->post($this->getCRPTDomain() . 'auth/cert/', [
+                RequestOptions::JSON => [
+                    'uuid' => $key['uuid'],
+                    'data' => $signed,
+                ],
+                'curl' => [
+                    CURLOPT_VERBOSE => true,
+                ],
+                'debug'              => true
+            ]);
+
+
+
+        print_r($jwt);exit;
+
     }
 }
