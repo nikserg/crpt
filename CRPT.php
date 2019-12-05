8fe4035c754271c82aac5b696217990b63e529f9
n.zarubin <n.zarubin@itsirius.ru>
2019-12-05 10:06:49 +0300

    guzzle

--- CRPT.php
+++ CRPT.php
@@ -1,4 +1,40 @@
 <?php
 namespace nikserg\crpt;
+
 class CRPT {
+    private $test;
+    private $jwt;
+    private $httpClient;
+
+    /**
+     * CRPT constructor.
+     *
+     * @param bool $test Отправлять запросы на тестовый контур ЦРПТ
+     */
+    function __construct($test = true)
+    {
+        $this->test = $test;
+    }
+
+    /**
+     * Домен для запросов к ЦРПТ
+     *
+     *
+     * @return string
+     */
+    private function getCRPTDomain()
+    {
+        if ($this->test) {
+            return 'https://demo.fashion.crpt.ru/api/v3/';
+        }
+        return 'https://ismp.crpt.ru/api/v3/';
+    }
+
+    public function auth()
+    {
+        //Уже авторизированы
+        if ($this->jwt) {
+            return true;
+        }
+    }
 }
