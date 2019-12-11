31f621afd693e0a626441c86a332cdcb29c5c640
Nikita Zarubin <nikita-dimitrov@mail.ru>
2019-12-11 16:50:40 +0300

    Update CRPT.php

--- CRPT.php
+++ CRPT.php
@@ -1,28 +1,99 @@
 <?php
+
 namespace nikserg\crpt;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use nikserg\crpt\exception\NotAuthException;
+use nikserg\crpt\exception\TokenExpiredException;
+use nikserg\crpt\schema\AuthData;
+use nikserg\crpt\schema\CodeInfo;
+use nikserg\crpt\schema\JWT;
 use nikserg\cryptoprocli\CryptoProCli;
 use yii\db\Exception;
 
-class CRPT {
+class CRPT
+{
     private $test;
+    /**
+     * @var JWT
+     */
     private $jwt;
     private $httpClient;
-    private $certificateThumbprint;
 
     /**
      * CRPT constructor.
      *
-     * @param string $certificateThumbprint SHA1-отпечаток сертификата для входа в ЦРПТ. Пример: 2b0b6f44b3507b509aaa0b2d38dc4819a557a40b
-     * @param bool $test Отправлять запросы на тестовый контур ЦРПТ
+     * @param string $certificateThumbprint SHA1-отпечаток сертификата для входа в ЦРПТ. Пример:
+     *     2b0b6f44b3507b509aaa0b2d38dc4819a557a40b
+     * @param bool   $test Отправлять запросы на тестовый контур ЦРПТ
      */
-    function __construct($certificateThumbprint, $test = true)
+    function __construct($test = true)
     {
         $this->test = $test;
-        $this->certificateThumbprint = $certificateThumbprint;
         $this->httpClient = new Client();
+    }
+
+    /**
+     * Получить JWT-токен в ЦРПТ
+     *
+     *
+     * @param string $certificateThumbprint SHA1-отпечаток сертификата для входа в ЦРПТ. Пример:
+     *     2b0b6f44b3507b509aaa0b2d38dc4819a557a40b
+     * @throws \Exception
+     * @return JWT
+     */
+    public function getJwt($certificateThumbprint)
+    {
+        //Получаем данные для подписи
+        $authData = $this->getAuthData();
+
+        //Подписываем
+        $signed = CryptoProCli::signData($authData->data, $certificateThumbprint);
+        $signed = str_replace("\n", '', $signed);
+
+        return $this->checkAuthData($authData->uuid, $signed);
+    }
+
+    /**
+     * @return AuthData
+     * @throws \Exception
+     */
+    public function getAuthData() {
+
+        $data = @json_decode($this->httpClient->get($this->getCRPTDomain() . 'auth/cert/key/')->getBody(), true);
+        if (!$data || !isset($data['data'])) {
+            throw new \Exception('Невозможно получить данные для подписи для аутентификации в ЦРПТ');
+        }
+        return new AuthData($data);
+    }
+
+
+    /**
+     * Проверить подписанные данные для авторизации
+     *
+     * @param string $uuid
+     * @param string $signedData
+     * @return JWT
+     * @throws \Exception
+     */
+    public function checkAuthData($uuid, $signedData)
+    {
+        //Отправляем подписанные данные для JWT-токена
+        $jwt = @json_decode($this->httpClient->post($this->getCRPTDomain() . 'auth/cert/', [
+            RequestOptions::JSON => [
+                'uuid' => $uuid,
+                'data' => $signedData,
+            ],
+        ])->getBody()->getContents(), true);
+
+        if (!$jwt || !isset($jwt['token'])) {
+            throw new \Exception('Невозможно получить JWT-токен в ЦРПТ с использованием указанной подписи');
+        }
+
+        //Парсим JWT
+        return JWT::parse($jwt['token']);
+
     }
 
     /**
@@ -39,39 +110,54 @@ class CRPT {
         return 'https://ismp.crpt.ru/api/v3/';
     }
 
-    public function auth()
+    /**
+     * Авторизация на сервере
+     *
+     * @param JWT $jwt
+     * @throws TokenExpiredException
+     */
+    public function auth($jwt)
     {
-        //Уже авторизированы
-        if ($this->jwt) {
-            return true;
+        if (!$jwt->isValid()) {
+            throw new TokenExpiredException('JWT-токен валиден только до '.$jwt->getValidTo()->format('d.m.Y H:i:s'));
         }
+        $this->jwt = $jwt;
+    }
 
-        //Получаем данные для подписи
-        $key = @json_decode($this->httpClient->get($this->getCRPTDomain() . 'auth/cert/key/')->getBody(), true);
+    /**
+     * Получить информацию о коде
+     *
+     * @param $code
+     * @return CodeInfo|null
+     * @throws NotAuthException
+     */
+    public function getCodeInfo($code)
+    {
+        $this->checkJwt();
+        $code = substr($code, 0, 31);
+        $info = @json_decode($this->httpClient->get($this->getCRPTDomain() . 'facade/identifytools/' . $code, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->jwt->token,
+            ],
+        ])->getBody()->getContents(), true);
 
-        //Подписываем
-        $signed = CryptoProCli::signData($key['data'], $this->certificateThumbprint);
-        $signed = str_replace("\n", '', $signed);
+        if (!$info) {
+            return null;
+        }
+        $return = new CodeInfo();
+        foreach ($info as $key => $value) {
+            $return->{$key} = $value;
+        }
+        return $return;
+    }
 
-        /*$json = json_encode([ 'uuid' => $key['uuid'],
-                              'data' => $signed,]);
-        die($json);*/
-        //Отправляем подписанное
-
-            $jwt = $this->httpClient->post($this->getCRPTDomain() . 'auth/cert/', [
-                RequestOptions::JSON => [
-                    'uuid' => $key['uuid'],
-                    'data' => $signed,
-                ],
-                'curl' => [
-                    CURLOPT_VERBOSE => true,
-                ],
-                'debug'              => true
-            ]);
-
-
-
-        print_r($jwt);exit;
-
+    private function checkJwt()
+    {
+        if (!$this->jwt) {
+            throw new NotAuthException();
+        }
+        if (!$this->jwt->isValid()) {
+            throw new TokenExpiredException();
+        }
     }
 }
